6aa9451aaae45905a5ea68c3f5e88ee76e751c19
Julian Mateo Ballesteros Rojas <mate_balles@hotmail.com>
2018-11-10 20:29:52 -0500

    SQLServer connection

---
+++ PetrichorPedidos/src/java/SQL/Conexion.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author mate_
+ */
+public class Conexion {
+    
+    public Connection connect(){
+        Connection con = null;
+        String URL = "jdbc:sqlserver://localhost:1433;databaseName = CinemaMorgan";
+        
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+             con = DriverManager.getConnection(URL, "Skrillmau", "1234");
+             System.out.println("Conexion con la base de datos establecida");
+            
+        }catch(ClassNotFoundException | SQLException e){
+            System.err.println("Error estableciendo conexion con la base de datos");
+            System.err.println(e.getMessage());
+        }
+        return con;
+    }
+
+    
+    public static void main(String[] args){
+        Conexion cone = new Conexion();
+        System.out.println("Conectando");
+        Connection conectar = cone.connect();
+    }
+            
+}
