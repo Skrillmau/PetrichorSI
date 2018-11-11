012096a7600516427a0dc543773ea79e0571aaf8
Julian Mateo Ballesteros Rojas <mate_balles@hotmail.com>
2018-11-10 20:42:17 -0500

    SQLServer Connection v2

--- PetrichorPedidos/src/java/SQL/Conexion.java
+++ PetrichorPedidos/src/java/SQL/Conexion.java
@@ -14,21 +14,21 @@ import java.sql.SQLException;
  * @author mate_
  */
 public class Conexion {
-    
-    public Connection connect(){
+
+    public Connection connect() {
         Connection con = null;
         String URL = "jdbc:sqlserver://localhost:1433;databaseName = CinemaMorgan";
-        
-        try{
+
+        try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-             con = DriverManager.getConnection(URL, "Skrillmau", "1234");
-             System.out.println("Conexion con la base de datos establecida");
-            
-        }catch(ClassNotFoundException | SQLException e){
+            con = DriverManager.getConnection(URL, "Skrillmau", "1234");
+            System.out.println("Conexion con la base de datos establecida");
+
+        } catch (ClassNotFoundException | SQLException e) {
             System.err.println("Error estableciendo conexion con la base de datos");
             System.err.println(e.getMessage());
         }
         return con;
     }
- 
+
 }
